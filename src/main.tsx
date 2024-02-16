f155111dfb6f0a6b6ef28f05dd389907d2e8b309
adamass <43586138+adamass@users.noreply.github.com>
2024-02-16 17:38:48 +0100

    Testing_1

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import App from "./App.tsx";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <GoogleOAuthProvider clientId="787437815662-fv28kp3ei1dvpla457470iof1i58m1kv.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="">
     <React.StrictMode>
       <ChakraProvider>
         <QueryClientProvider client={queryClient}>
